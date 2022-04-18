f445c09cf65d8374ee90d15222a9c537bb594f8c
abhayparopate23 <102666071+abhayparopate23@users.noreply.github.com>
2022-04-18 14:59:09 +0530

    subtraction of two digit number

---
+++ subtraction of two number.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int num1,num2,sub;
+	printf("enter two numbers");
+	scanf("%d%d",&num1,&num2);
+	sub=num1-num2;
+	printf("subtrction of two numbers is=%d",sub);
+	getch();
+}
