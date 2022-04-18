75cfc9442fe9cbd66f44d3ece72b7aa3bd821ccc
abhayparopate23 <102666071+abhayparopate23@users.noreply.github.com>
2022-04-18 15:34:07 +0530

    area of rectangle

---
+++ area of rec.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num1,num2,area;
+printf("enter length,breadth \n");
+scanf("%d%d",&num1,&num2);
+area=num1*num2;
+printf("area is=%d",area);
+getch();
+}
+
