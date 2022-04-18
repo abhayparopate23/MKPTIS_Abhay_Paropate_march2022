8fe3c5ea8057f194e08c85ed3468db56d3ed1912
abhayparopate23 <102666071+abhayparopate23@users.noreply.github.com>
2022-04-18 15:07:41 +0530

    division of two number

---
+++ divsion of two number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int num1,num2,div;
+	printf("enter two numbers");
+	scanf("%d%d",&num1,&num2);
+	div=num1/num2;
+	printf("division of two numbers is=%d",div);
+	getch();
+}
