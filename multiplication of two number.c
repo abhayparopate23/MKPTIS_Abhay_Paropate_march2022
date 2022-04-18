41a1a316fafa2cd96bc33813841e29ca39be3460
abhayparopate23 <102666071+abhayparopate23@users.noreply.github.com>
2022-04-18 15:00:30 +0530

    multiplication of two numbers

---
+++ multiplication of two number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int num1,num2,mul;
+	printf("enter two numbers");
+	scanf("%d%d",&num1,&num2);
+	mul=num1*num2;
+	printf("multiplication of two numbers is=%d",mul);
+	getch();
+}
