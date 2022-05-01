028827253fbb44b5149ab0d12c750856a6c67847
abhayparopate23 <102666071+abhayparopate23@users.noreply.github.com>
2022-05-01 15:13:04 +0530

    perimeter and area of rectangle

---
+++ A109.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float p=3.14,r=6,a,pe;
+a=p*r*r;
+pe=2*p*r;
+printf("area of circle= %f inches\n",a);
+printf("perimeter of circle= %f inches",pe);
+getch();
+}
