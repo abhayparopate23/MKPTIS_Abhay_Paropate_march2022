c4306bbe389d55ecf122b4ffc2c50e7f71f3a014
abhayparopate23 <102666071+abhayparopate23@users.noreply.github.com>
2022-05-01 15:11:48 +0530

    area and perimeter

---
+++ A108.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int h=7,b=5,area,peri;
+area=h*b;
+peri=2*(h+b);
+printf("area of rectangle= %d inches\n",area);
+printf("perimeter of rectangle= %d inches",peri);
+getch();
+}
